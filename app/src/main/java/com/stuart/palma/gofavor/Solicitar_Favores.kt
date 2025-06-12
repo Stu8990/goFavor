0df6d4b09ea400ea41b7658f5fc5068691559d5e
Ariel Justin Amaguaña Toapanta <ariel.amaguana@epn.edu.ec>
2025-06-12 06:11:52 -0500

    add: Agregando el resto de pantallas

---
+++ app/src/main/java/com/stuart/palma/gofavor/Solicitar_Favores.kt
@@ -0,0 +1,20 @@
+package com.stuart.palma.gofavor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Solicitar_Favores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_solicitar_favores)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
